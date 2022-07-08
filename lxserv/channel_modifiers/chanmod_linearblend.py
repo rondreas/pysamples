f60b6151b47658bcb7d26868bc5a30d4abfd4cb1
Andreas Rånman <rondreas@users.noreply.github.com>
2022-07-08 13:04:08 +0200

    Update chanmod_linearblend.py
    Added docstrings from lxu_attrdesc.hpp and replaces set_hint with setting min and max allowed value to the channel

--- lxserv/channel_modifiers/chanmod_linearblend.py
+++ lxserv/channel_modifiers/chanmod_linearblend.py
@@ -6,11 +6,15 @@
 
 import lx
 import lxu.meta
+import lxu.attrdesc
 
 
 class Operator(lxu.meta.ChannelModifier):
-    def init_chan(self, desc):
+    def init_chan(self, desc: lxu.attrdesc.AttributeDesc):
+        # Call add() to add a new attribute with a name and type. This becomes
+        # the current attribute and further methods can customize the attribute.
         desc.add("inputA", lx.symbol.sTYPE_FLOAT)
+        # Set the channel as an input or output of a channel modifier.
         desc.chmod_value(lx.symbol.fCHMOD_INPUT)
 
         desc.add("inputB", lx.symbol.sTYPE_FLOAT)
@@ -18,7 +22,11 @@ class Operator(lxu.meta.ChannelModifier):
 
         desc.add("blend", lx.symbol.sTYPE_PERCENT)
         desc.chmod_value(lx.symbol.fCHMOD_INPUT)
-        desc.set_hint([(0, "%min"), (10000, "%max")])
+        # Set the text hints for encoding int values.
+        # The C++ calls the "older" api .hint(...) which basically just calls the newer set_hint
+        # desc.set_hint([(0, "%min"), (1000, "%max")]) # <-- Does nothing that I can tell
+        desc.set_min(0.0)
+        desc.set_max(1.0)
 
         desc.add("output", lx.symbol.sTYPE_FLOAT)
         desc.chmod_value(lx.symbol.fCHMOD_OUTPUT)
